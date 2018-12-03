069402f57912c02c4e166c056ab19ab3497c8d03
Jérôme <jer.vignerot@gmail.com>
2018-12-03 09:49:35 +0100

    Queste Symphony.

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,246 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Tag;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/blog")
+ */
+class BlogController extends AbstractController
+{
+    /**
+     * @var CategoryRepository
+     */
+    private $categoryRepository;
+
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+
+    /**
+     * BlogController constructor.
+     *
+     * @param CategoryRepository $categoryRepository
+     * @param ObjectManager $manager
+     */
+    public function __construct(CategoryRepository $categoryRepository, ObjectManager $manager)
+    {
+        $this->categoryRepository = $categoryRepository;
+        $this->manager = $manager;
+    }
+
+    /**
+     * Show all row from category's entity
+     *
+     * @Route("/", name="blog_index")
+     * @return Response A response instance
+     */
+    public function index() : Response
+    {
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+        if (!$categories) {
+            throw $this->createNotFoundException(
+                'No Category found in category\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/index.html.twig',
+            ['categories' => $categories]
+        );
+    }
+
+    /**
+     * Getting a article with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     *
+     * @Route("/{slug<^[a-z0-9- ]+$/>}",
+     *     defaults={"slug" = null},
+     *     name="blog_slug")
+     *
+     * @return Response A response instance
+     */
+    public function show($slug) : Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with '.$slug.' title, found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/showArticle.html.twig',
+            [
+                'article' => $article,
+                'slug' => $slug
+            ]
+        );
+    }
+
+    /**
+     * Show all row from category's entity
+     *
+     * @Route("/category/{category}", name="blog_show_category")
+     *
+     * @param string $category
+     *
+     * @return Response A response instance
+     */
+    public function showByCategory(string $category): Response
+    {
+        $categoryRepository = $this->getDoctrine()
+            ->getRepository(Category::class);
+
+        $articleRepository = $this->getDoctrine()
+            ->getRepository(Article::class);
+
+        $categoryFind = $categoryRepository->findOneByName($category);
+        $articleFind = $articleRepository->findBy(
+            ["category" => $categoryFind->getId()],
+            ["id" => 'DESC'],
+            3);
+
+        return $this->render('blog/category.html.twig', [
+            'articles' => $articleFind,
+            'category' => $categoryFind
+        ]);
+    }
+
+    /**
+     * Getting a article with a for id
+     *
+     * @Route("/category/article/{id}", defaults={"id" = null}, name="blog_show_article")
+     *
+     * @param Article $id
+     *
+     * @return Response A response instance
+     */
+    public function showByArticle(Article $id): Response
+    {
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->find($id);
+
+        $tags = $article->getTags();
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with '.$article.' title, found in article\'s table.'
+            );
+        }
+        return $this->render(
+            'blog/showArticle.html.twig',
+            [
+                'article' => $article,
+                'tags' => $tags
+            ]
+        );
+
+    }
+
+    /**
+     * Show all row from tag's entity
+     *
+     * @Route("/tag", name="blog_showAll_tag")
+     *
+     * @return Response A response instance
+     */
+    public function showAllTag(): Response
+    {
+        $tags = $this->getDoctrine()
+            ->getRepository(Tag::class)
+            ->findAll();
+
+        if (!$tags ) {
+            throw $this->createNotFoundException(
+                'No Category found in tag\'s table.'
+            );
+        }
+        return $this->render(
+            'blog/showAllTag.html.twig',
+            [
+                'tags' => $tags
+            ]
+        );
+
+    }
+
+    /**
+     * Show all row from category's entity
+     *
+     * @Route("/tag/{name}", defaults={"name" = null}, name="blog_show_tag")
+     *
+     * @param string $name
+     *
+     * @return Response A response instance
+     */
+    public function showByTag(string $name): Response
+    {
+        $tag = $this->getDoctrine()
+            ->getRepository(Tag::class)->findOneByName($name);
+
+        $articles = $tag->getArticles();
+
+        return $this->render('blog/showTag.html.twig',
+            [
+                'articles' => $articles,
+                'tag' => $tag
+            ]
+        );
+    }
+
+    /**
+     * Creating a category
+     *
+     * @Route ("/category", name="category_new")
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function newCategory(Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($category);
+            $this->manager->flush();
+            $this->addFlash('success', 'Catégorie créée avec succès');
+            return $this->redirectToRoute('blog_index');
+        }
+        return $this->render('blog/newCategory.html.twig', [
+            'category' => $category,
+            'form'     => $form->createView()
+        ]);
+    }
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Tag", mappedBy="articles")
+     */
+    private $tags;
+
+    public function __construct()
+    {
+        $this->tags = new ArrayCollection();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Article
+     */
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     * @return Article
+     */
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * @return Category|null
+     */
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param Category|null $category
+     * @return Article
+     */
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Tag[]
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): self
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags[] = $tag;
+            $tag->addArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): self
+    {
+        if ($this->tags->contains($tag)) {
+            $this->tags->removeElement($tag);
+            $tag->removeArticle($this);
+        }
+
+        return $this;
+    }
+
+}
